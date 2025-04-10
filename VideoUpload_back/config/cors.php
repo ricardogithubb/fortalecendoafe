b345ee114547d03e88915027f35539d1675f9136
Ricardo A. <ricardoal.tim@gmail.com>
2025-04-10 11:53:37 -0300

    Subindo restante conteudo

--- VideoUpload_back/config/cors.php
+++ VideoUpload_back/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://127.0.0.1:5500','https://fortalecendoafe.markethubplace.com'],
+    'allowed_origins' => ['http://127.0.0.1:5500','https://fortalecendoafe.markethubplace.com','https://ricardogithubb.github.io/fortalecendoafe'],
 
     'allowed_origins_patterns' => [],
 
